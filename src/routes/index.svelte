7473d691f3e38e6ae67fa2ee56b359a0e6fc855b
daseul22 <tlaektmf27@gmail.com>
2021-02-19 10:04:26 +0900

    Test: commit.sh

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -47,5 +47,5 @@
 	<figcaption>Have fun with Sapper!</figcaption>
 </figure>
 
-<p><strong>Try editing this file (src/routes/index.svelte) to test live RELODING.</strong></p>
+<p><strong>Try editing this file (src/routes/index.svelte) to test live Reloading.</strong></p>
 
