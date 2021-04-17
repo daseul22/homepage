b772172ef78bb8cb44ae8ac6a668f7a365dc268b
daseul22 <tlaektmf27@gmail.com>
2021-04-18 00:26:55 +0900

    chore: init

---
+++ src/components/Body.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { link } from "svelte-spa-router"
+	import active from "svelte-spa-router/active"
+</script>
+
+<style lang="scss">
+	#app-body {
+		max-width: 940px;
+		margin: 0 auto;
+	}
+</style>
+
+<div id="app-body">
+	<slot />
+</div>
---
+++ src/routes/BackEnd.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { link } from "svelte-spa-router"
+	import active from "svelte-spa-router/active"
+</script>
+
+<style lang="scss">
+</style>
+
+<div>Back-end</div>
